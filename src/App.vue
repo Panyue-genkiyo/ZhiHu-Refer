6e168fd12e1ed6e59fddcc9739a5b036721531ca
panyue <90938238g@gmail.com>
2022-01-01 23:43:08 +0800

    全局app组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+   <div class="container">
+     <global-header :user="currentUser"/>
+     <loader v-if="isLoading"/>
+     <router-view></router-view>
+     <footer class="text-center py-4 text-secondary bg-light mt-6">
+         <small>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">© 2022 py专栏</li>
+              <li class="list-inline-item">课程</li>
+              <li class="list-inline-item">文档</li>
+              <li class="list-inline-item">联系</li>
+              <li class="list-inline-item">更多</li>
+            </ul>
+         </small>
+     </footer>
+   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import { defineComponent, computed, watch, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import GlobalHeader from '@/components/GlobalHeader.vue'
+import Loader from '@/components/Loader.vue'
+import createMessage from '@/utils/createMessage'
+import axios from 'axios'
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    // ColumnList,
+    GlobalHeader,
+    Loader,
+  },
+  setup() {
+    const store = useStore();
+    const currentUser = computed(() => store.state.user);
+    const isLoading = computed(() => store.state.loading);
+    const error = computed(() => store.state.error);
+    const token = computed(() => store.state.token)
+
+    onMounted(() => {
+       if(!currentUser.value.isLogin && token.value){
+         axios.defaults.headers.common.Authorization = `Bearer ${token.value}`
+         store.dispatch('fetchCurrentUser');
+       }
+    })
+
+    watch(() => error.value.status, () => {
+      const { status, message } = error.value;
+      if(status && message){
+        createMessage(message, 'error');
+      }
+    })
+    return {
+      currentUser,
+      isLoading,
+      error
+    }
   }
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
