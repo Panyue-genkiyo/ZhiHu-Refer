a32406018962a250de5d51e270b746fbcb18bbba
panyue <90938238g@gmail.com>
2022-01-01 23:46:03 +0800

    vue配置文件(解决跨域问题)

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    open: true,
+    hotOnly: true,
+    proxy: {
+      '/api': {
+        target: 'http://api.vikingship.xyz/api',//跨域接口的地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  }
+};
