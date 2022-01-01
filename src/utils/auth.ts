ad96c5dc01ef556084fc36db078c6f5f4ea7cd01
panyue <90938238g@gmail.com>
2022-01-01 23:40:57 +0800

    封装token删除|重置|获取

---
+++ src/utils/auth.ts
@@ -0,0 +1,12 @@
+//处理token
+export const getToken = () => {
+  return localStorage.getItem('token') || '';
+}
+
+export const setToken = (token: string) => {
+  localStorage.setItem('token', token);
+}
+
+export const removeToken = () => {
+  localStorage.removeItem('token');
+}
