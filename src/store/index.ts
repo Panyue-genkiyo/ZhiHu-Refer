74c5f77b8db14bf88804f371be1ea231befc295b
panyue <90938238g@gmail.com>
2022-03-04 15:54:46 +0800

    全局状态管理文件

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,19 @@
 import { createStore, Commit } from 'vuex'
-import axios from 'axios'
+import axios,{ AxiosRequestConfig }   from 'axios'
+import { arrToObj, objToArr } from '@/utils/helper'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 
-interface ImageProps{
-  _id?: string,
-  url?: string,
-  createdAt?:string
+export interface ResponseType<P = {}> {
+  code: number;
+  msg: string;
+  data: P;
+}
+
+export interface ImageProps{
+  _id?: string;
+  url?: string;
+  createdAt?: string;
+  fitUrl?: string;
 }
 
 export interface ColumnProps {
@@ -16,21 +24,25 @@ export interface ColumnProps {
 }
 
 export interface PostProps {
-  _id: string;
+  _id?: string;
   title: string;
   excerpt?: string;
   content?: string;
-  image?: ImageProps;
-  createdAt: string;
+  image?: ImageProps | string;
+  createdAt?: string;
   column: string;
+  author?: string | UserProps;
+  isHTML?: boolean;
 }
 
 export interface UserProps{
-  isLogin: boolean,
-  nickName?: string,
-  _id?: string,
-  column?: string,
-  email?:string,
+  isLogin: boolean;
+  nickName?: string;
+  _id?: string;
+  column?: string;
+  email?: string;
+  avatar?: ImageProps;
+  description?: string;
 }
 
 export interface GlobalErrorProps{
@@ -38,122 +50,163 @@ export interface GlobalErrorProps{
   message?: string
 }
 
+interface ListProps<P> {
+  [id: string]: P;
+}
+
 export interface GlobalState {
   error: GlobalErrorProps,
   token:string,
   loading:boolean,
-  user: UserProps,
-  posts: PostProps[],
-  columns: ColumnProps[]
+  columns: { data: ListProps<ColumnProps>; currentPage: number; total: number };
+  posts: { data: ListProps<PostProps>; loadedColumns: string[] };
+  user: UserProps;
 }
 
 
-const getAndCommit = async (url: string, mutationName: string, commit:Commit) => {
-  const { data } = await axios.get(url);
-  commit(mutationName, data);
+const asyncAndCommit = async(
+  url: string,
+  mutationName: string,
+  commit: Commit,
+  config: AxiosRequestConfig = { method: 'get' },
+  extraData?: any) => {
+  const { data } = await axios(url, config)
+  if (extraData) {
+    commit(mutationName, { data, extraData })
+  } else {
+    commit(mutationName, data)
+  }
+  return data
 }
-
-const postAndCommit = async (url: string, mutationName: string, commit:Commit, payload:any) => {
-  const { data } = await axios.post(url, payload);
-  console.log(data);
-  commit(mutationName, data);
-  return data;
-}
-
-
 const store = createStore<GlobalState>({
-  state:{
-     error:{
-       status:false
-     },
-     token: getToken(),
-     loading:false,
-     columns: [],
-     posts: [],
-     user: {
-       isLogin: false,
-     }
+  state: {
+    token: getToken(),
+    error: { status: false },
+    loading: false,
+    columns: { data: {}, currentPage: 0, total: 0 },
+    posts: { data: {}, loadedColumns: [] },
+    user: { isLogin: false }
   },
-  mutations:{
-    //mutation只能是同步操作
-    // login(state){
-    //   state.user = {...state.user, isLogin: true, name: 'viking', columnId:1}
+  mutations: {
+    // login(state) {
+    //   state.user = { ...state.user, isLogin: true, name: 'viking' }
     // },
-    createPost(state, newPost){
-      state.posts.push(newPost);
+    createPost(state, newPost) {
+      state.posts.data[newPost._id] = newPost
     },
-    fetchColumns(state, columns){
-      state.columns = columns.data.list;
+    fetchColumns(state, rawData) {
+      const { data } = state.columns;
+      const { list, count, currentPage } = rawData.data;
+      state.columns = {
+        data: { ...data, ...arrToObj(list) },
+        total: count,
+        currentPage: currentPage * 1
+      }
     },
-    fetchColumn(state, column){
-      state.columns = [column.data];
+    fetchColumn(state, rawData) {
+      state.columns.data[rawData.data._id] = rawData.data;
     },
-    fetchPosts(state, posts){
-      state.posts = posts.data.list;
+    fetchPosts(state, { data: rawData, extraData: columnId }) {
+      state.posts.data = { ...state.posts.data, ...arrToObj(rawData.data.list) };
+      state.posts.loadedColumns.push(columnId);
     },
-    setLoading(state, loading){
-      state.loading = loading;
+    fetchPost(state, rawData) {
+      state.posts.data[rawData.data._id] = rawData.data;
     },
-    login(state, user){
-       const { token } = user.data;
-       state.token = token;
-      //持久化信息(localstorage)
-       setToken(token);
-       axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    deletePost(state, { data }) {
+      delete state.posts.data[data._id];
     },
-    logout(state){
-       state.token = '';
-       state.user = {isLogin: false};
-       removeToken();
+    updatePost(state, { data }) {
+      state.posts.data[data._id] = data;
     },
-    fetchCurrentUser(state, currentUser){
-       state.user = { isLogin: true, ...currentUser.data };
+    setLoading(state, status) {
+      state.loading = status;
     },
-    setError(state, e: GlobalErrorProps){
+    setError(state, e: GlobalErrorProps) {
       state.error = e;
     },
+    fetchCurrentUser(state, rawData) {
+      state.user = { isLogin: true, ...rawData.data };
+    },
+    login(state, rawData) {
+      const { token } = rawData.data;
+      state.token = token;
+      setToken(token);
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    },
+    logout(state) {
+      state.token = '';
+      state.user = { isLogin: false };
+      removeToken();
+      delete axios.defaults.headers.common.Authorization;
+    }
   },
-  actions:{
-    //action可以是异步操作 同时发出一个mutation
-    fetchColumns({ commit }) {
-      //发送异步请求获取列表数据
-      getAndCommit('/api/columns', 'fetchColumns', commit);
+  actions: {
+    fetchColumns({ state, commit }, params = {}) {
+      const { currentPage = 1, pageSize = 6 } = params;
+      // if (!state.columns.isLoaded) {
+      //   return asyncAndCommit('/columns', 'fetchColumns', commit)
+      // }
+      if (state.columns.currentPage < currentPage) {
+        return asyncAndCommit(`/api/columns?currentPage=${currentPage}&pageSize=${pageSize}`, 'fetchColumns', commit)
+      }
     },
-    //获取单个专栏
-    fetchColumn({ commit }, cid){
-      getAndCommit(`/api/columns/${cid}`, 'fetchColumn', commit);
+    fetchColumn({ state, commit }, cid) {
+      if (!state.columns.data[cid]) {
+        return asyncAndCommit(`/api/columns/${cid}`, 'fetchColumn', commit)
+      }
     },
-    //获取单个专栏下的post
-    fetchPosts({ commit }, cid){
-      getAndCommit(`/api/columns/${cid}/posts`, 'fetchPosts', commit);
+    fetchPosts({ state, commit }, cid) {
+      if (!state.posts.loadedColumns.includes(cid)) {
+        return asyncAndCommit(`/api/columns/${cid}/posts`, 'fetchPosts', commit, { method: 'get' }, cid)
+      }
     },
-    //获取当前用户
-    fetchCurrentUser({ commit }){
-      getAndCommit('/api/user/current', 'fetchCurrentUser', commit);
+    fetchPost({ state, commit }, id) {
+      const currentPost = state.posts.data[id];
+      if (!currentPost || !currentPost.content) {
+        return asyncAndCommit(`/api/posts/${id}`, 'fetchPost', commit)
+      } else {
+        return Promise.resolve({ data: currentPost });
+      }
     },
-    login({ commit }, payload){
-      return postAndCommit('/api/user/login', 'login', commit, payload);
+    updatePost({ commit }, { id, payload }) {
+      return asyncAndCommit(`/api/posts/${id}`, 'updatePost', commit, {
+        method: 'patch',
+        data: payload
+      });
     },
-    //组合action(login and fetch)
-    loginAndFetch({ dispatch }, loginData){
+    fetchCurrentUser({ commit }) {
+      return asyncAndCommit('/api/user/current', 'fetchCurrentUser', commit);
+    },
+    login({ commit }, payload) {
+      return asyncAndCommit('/api/user/login', 'login', commit, { method: 'post', data: payload });
+    },
+    createPost({ commit }, payload) {
+      return asyncAndCommit('/api/posts', 'createPost', commit, { method: 'post', data: payload });
+    },
+    deletePost({ commit }, id) {
+      return asyncAndCommit(`/api/posts/${id}`, 'deletePost', commit, { method: 'delete' });
+    },
+    loginAndFetch({ dispatch }, loginData) {
       return dispatch('login', loginData).then(() => {
-        return dispatch('fetchCurrentUser')
+        return dispatch('fetchCurrentUser');
       })
     }
   },
-  getters:{
-    getColumnById(state){
-      //高阶函数
-      return (id: string) => {
-        return state.columns.find(column => column._id === id)
-      }
+  getters: {
+    getColumns: (state) => {
+      return objToArr(state.columns.data)
     },
-    getPostsByCid(state){
-      return (cid: string) => {
-        return state.posts.filter(post => post.column === cid )
-      }
+    getColumnById: (state) => (id: string) => {
+      return state.columns.data[id]
+    },
+    getPostsByCid: (state) => (cid: string) => {
+      return objToArr(state.posts.data).filter(post => post.column === cid)
+    },
+    getCurrentPost: (state) => (id: string) => {
+      return state.posts.data[id]
     }
   }
-});
+})
 
 export default store
