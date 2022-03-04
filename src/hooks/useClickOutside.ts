09eb4eb53557b41d6eec67a650297bdd0a487819
panyue <90938238g@gmail.com>
2022-03-04 15:53:36 +0800

    自定义hook(当鼠标点击外侧自动关闭下拉菜单)

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,19 +1,25 @@
 import { ref, onMounted, onUnmounted, Ref } from 'vue'
 
 const useClickOutside = (elementRef: Ref<null | HTMLElement>) => {
-   const isClickOutside = ref(false);
-   const handler = (event: MouseEvent) => {
-     if (elementRef.value) {
-       isClickOutside.value = !elementRef.value.contains(event.target as HTMLElement);
-     }
-   };
-   onMounted(() => {
-      document.addEventListener('click', handler);
-   })
-   onUnmounted(() => {
-     document.removeEventListener('click', handler);
-   })
-   return isClickOutside;
+  const isClickOutside = ref(false)
+  const handler = (e: MouseEvent) => {
+    if (elementRef.value) {
+      /**
+       * node.contains(otherNode)
+       * node是否包含otherNode节点
+       * otherNode是否是node的后代节点
+       * 如果otherNode是node的后代节点或是node节点本身，则返回true，否则返回false
+       */
+      isClickOutside.value = !elementRef.value.contains(e.target as HTMLElement);
+    }
+  }
+  onMounted(() => {
+    document.addEventListener('click', handler)
+  })
+  onUnmounted(() => {
+    document.removeEventListener('click', handler)
+  })
+  return isClickOutside
 }
 
 export default useClickOutside
