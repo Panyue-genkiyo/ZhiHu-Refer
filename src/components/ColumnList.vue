54902999df284f2d3d8cdbb7b47d7f8e0d5ad7dd
panyue <90938238g@gmail.com>
2022-03-04 15:50:18 +0800

    列表组件

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -3,10 +3,10 @@
     <div v-for="column in columnList" :key="column._id" class="col-4 mb-4">
       <div class="card h-100 shadow-sm">
         <div class="card-body text-center">
-          <img :src="column.avatar?.url" :alt="column.title" class="rounded-circle border border-light  my-3" >
+          <img :src="column.avatar && column.avatar.fitUrl" :alt="column.title" class="rounded-circle border border-light my-3" >
           <h5 class="card-title">{{column.title}}</h5>
           <p class="card-text text-left">{{column.description}}</p>
-          <router-link  :to="`/column/${column._id}`" class="btn btn-outline-primary">进入专栏</router-link>
+          <router-link :to="`/column/${column._id}`" class="btn btn-outline-primary">进入专栏</router-link>
         </div>
       </div>
     </div>
@@ -16,28 +16,23 @@
 <script lang="ts">
 
 import { defineComponent, PropType, computed } from 'vue'
-import { ColumnProps } from '@/store';
+import { ColumnProps } from '@/store'
+import { addColumnAvatar } from '@/utils/helper'
 
 export default defineComponent({
   name: 'ColumnList',
   props: {
-    list : {
+    list: {
       type: Array as PropType<ColumnProps[]>,
       required: true
     }
   },
-  setup(props){
+  setup(props) {
     const columnList = computed(() => {
-       return props.list.map(c => {
-         if(!c.avatar){
-           c.avatar = {
-              url: require('@/assets/column.png')
-           }
-         }else{
-           c.avatar.url = c.avatar.url + '?x-oss-process=image/resize,m_pad,h_50,w_50'
-         }
-         return c;
-       })
+      return props.list.map(column => {
+        addColumnAvatar(column, 50, 50)
+        return column
+      })
     })
     return {
       columnList
@@ -45,10 +40,9 @@ export default defineComponent({
   }
 })
 </script>
-
 <style scoped>
-  .card-body img{
-    width: 50px;
-    height: 50px;
-  }
+.card-body img {
+  width: 50px;
+  height: 50px;
+}
 </style>
