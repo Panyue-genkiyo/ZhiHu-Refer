6131af522beaa655621767840ff5217d610a3c0f
panyue <90938238g@gmail.com>
2022-03-04 15:51:54 +0800

    post列表组件

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,40 +2,37 @@
   <div class="post-list">
     <article v-for="post in posts" :key="post._id" class="card mb-3 shadow-sm">
       <div class="card-body">
-        <h4>
-          <router-link :to="`/posts/${post._id}/`">
-            {{ post.title }}
-          </router-link>
-        </h4>
+        <h4><router-link :to="`/posts/${post._id}/`">{{post.title}}</router-link></h4>
         <div class="row my-3 align-items-center">
           <div v-if="post.image" class="col-4">
-            <img :src="post.image.url" :alt="post.title" class="rounded-lg w-100" />
+            <img :src="post.image.fitUrl" :alt="post.title" class="rounded-lg w-100">
           </div>
-          <p :class="{ 'col-8': post.image }" class="text-muted">
-            {{ post.excerpt }}
-          </p>
+          <p :class="{'col-8': post.image}" class="text-muted">{{post.excerpt}}</p>
         </div>
-        <span class="text-muted">{{ post.createdAt }}</span>
+        <span class="text-muted">{{post.createdAt}}</span>
       </div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from 'vue'
-import { PostProps } from '@/store'
-
+import { defineComponent, PropType, computed } from 'vue'
+import { PostProps, ImageProps } from '@/store'
+import { generateFitUrl } from '@/utils/helper'
 export default defineComponent({
   props: {
     list: {
       required: true,
-      type: Array as PropType<PostProps[]>,
-    },
+      type: Array as PropType<PostProps[]>
+    }
   },
-  setup(props){
+  setup(props) {
     const posts = computed(() => {
-      return props.list.map(p => p)
-    });
+      return props.list.map(post => {
+        generateFitUrl(post.image as ImageProps, 200, 110, ['m_fill'])
+        return post
+      })
+    })
     return {
       posts
     }
@@ -46,9 +43,9 @@ export default defineComponent({
 <style scoped>
 .post-list h4 a {
   text-decoration: none;
-  color: #1a1a1a;
+  color:#1a1a1a;
 }
 .post-list h4 a:hover {
-  color: #0d6efd;
+  color:#0d6efd;
 }
 </style>
