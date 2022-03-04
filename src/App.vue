25a528b9ff323a342ceefaa15f66da14098b432a
panyue <90938238g@gmail.com>
2022-03-04 15:58:20 +0800

    app组件(总组件)

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, watch, onMounted } from 'vue'
+import { defineComponent, computed, watch } from 'vue'
 import { useStore } from 'vuex'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalHeader from '@/components/GlobalHeader.vue'
 import Loader from '@/components/Loader.vue'
 import createMessage from '@/utils/createMessage'
-import axios from 'axios'
+import { GlobalState } from '@/store'
 export default defineComponent({
   name: 'App',
   components: {
@@ -33,23 +33,14 @@ export default defineComponent({
     Loader,
   },
   setup() {
-    const store = useStore();
-    const currentUser = computed(() => store.state.user);
-    const isLoading = computed(() => store.state.loading);
-    const error = computed(() => store.state.error);
-    const token = computed(() => store.state.token)
-
-    onMounted(() => {
-       if(!currentUser.value.isLogin && token.value){
-         axios.defaults.headers.common.Authorization = `Bearer ${token.value}`
-         store.dispatch('fetchCurrentUser');
-       }
-    })
-
+    const store = useStore<GlobalState>()
+    const currentUser = computed(() => store.state.user)
+    const isLoading = computed(() => store.state.loading)
+    const error = computed(() => store.state.error)
     watch(() => error.value.status, () => {
-      const { status, message } = error.value;
-      if(status && message){
-        createMessage(message, 'error');
+      const { status, message } = error.value
+      if (status && message) {
+        createMessage(message, 'error')
       }
     })
     return {
