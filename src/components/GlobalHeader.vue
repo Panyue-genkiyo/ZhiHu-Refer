4fb64b810a7ae78592c4e5cfb00cfd4827c52fab
panyue <90938238g@gmail.com>
2022-03-04 15:50:52 +0800

    header头部组件

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,16 +1,17 @@
 <template>
   <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
-    <router-link class="navbar-brand" to="/">py专栏</router-link>
+    <router-link class="navbar-brand" to="/">者也专栏</router-link>
     <ul v-if="!user.isLogin" class="list-inline mb-0">
-      <li class="list-inline-item"><router-link to="/login" class="btn btn-outline-light my-2">登录</router-link></li>
+      <li class="list-inline-item"><router-link to="/login" class="btn btn-outline-light my-2">登陆</router-link></li>
       <li class="list-inline-item"><router-link to="/signup" class="btn btn-outline-light my-2">注册</router-link></li>
     </ul>
     <ul v-else class="list-inline mb-0">
       <li class="list-inline-item">
-        <dropdown :title="`你好,${user.nickName}`">
-          <dropdown-item><a href="#" class="dropdown-item">新建文章</a></dropdown-item>
-          <dropdown-item disabled><a href="#" class="dropdown-item">编辑资料</a></dropdown-item>
-          <dropdown-item><a href="#" @click.prevent="handlerLogout" class="dropdown-item">退出登录 </a></dropdown-item>
+        <dropdown :title="`你好 ${user.nickName}`">
+          <dropdown-item><router-link to="/create" class="dropdown-item">新建文章</router-link></dropdown-item>
+          <dropdown-item><router-link :to="`/column/${user.column}`" class="dropdown-item">我的专栏</router-link></dropdown-item>
+          <dropdown-item disabled><router-link to="/#" class="dropdown-item">编辑资料</router-link></dropdown-item>
+          <dropdown-item><a href="#" class="dropdown-item" @click.prevent="logout">退出登陆</a></dropdown-item>
         </dropdown>
       </li>
     </ul>
@@ -18,39 +19,39 @@
 </template>
 
 <script lang="ts">
-   import { defineComponent, PropType } from 'vue'
-   import Dropdown from '@/components/Dropdown.vue'
-   import DropdownItem from '@/components/DropdownItem.vue'
-   import { useStore } from 'vuex'
-   import { GlobalState } from '@/store'
-   import { UserProps} from '@/store'
-   import { useRouter } from 'vue-router'
-   import createMessage from '@/utils/createMessage'
-   export default defineComponent({
-     name: 'GlobalHeader',
-     components:{
-       Dropdown,
-       DropdownItem
-     },
-     props:{
-       user: {
-         type: Object as PropType<UserProps>,
-         required: true
-       },
-     },
-     setup(){
-       const router = useRouter();
-       const store = useStore<GlobalState>();
-       const handlerLogout = () => {
-         store.commit('logout');
-         createMessage('退出登录成功,两秒后跳转到首页', 'success');
-         setTimeout(() => {
-           router.push('/');
-         },2000);
-       }
-       return {
-         handlerLogout
-       }
-     }
-   })
+import { defineComponent, PropType } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
+import { UserProps, GlobalState } from '@/store'
+import createMessage from '@/utils/createMessage'
+
+export default defineComponent({
+  name: 'GlobalHeader',
+  components: {
+    Dropdown,
+    DropdownItem
+  },
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  },
+  setup(){
+    const store = useStore<GlobalState>();
+    const router = useRouter();
+    const logout = () => {
+        store.commit('logout');
+        createMessage('退出成功,两秒跳回登录页面', 'success');
+        setTimeout(() => {
+          router.push('/login');
+        },2000)
+    }
+    return {
+      logout,
+    }
+  }
+})
 </script>
