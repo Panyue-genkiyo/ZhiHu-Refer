c7094aafdb187827b1449a0bf48c8a60e7f0d75f
panyue <90938238g@gmail.com>
2022-03-04 15:51:19 +0800

    加载组件

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,46 +1,49 @@
 <template>
   <teleport to="#back">
-    <div
-      class="d-flex justify-content-center align-items-center h-100 loading-container"
-      :style="{backgroundColor: background || ''}"
-    >
+    <div class="d-flex justify-content-center align-items-center h-100 w-100 loading-container"
+         :style="{backgroundColor: background || ''}">
       <div class="loading-content">
         <div class="spinner-border text-primary" role="status">
-          <span class="sr-only"></span>
+          <span class="sr-only">{{ text }}</span>
         </div>
-        <p v-if="text" class="text-primary samll">{{ text }}</p>
+        <p v-if="text" class="text-primary small">{{text}}</p>
       </div>
     </div>
   </teleport>
 </template>
 
 <script lang="ts">
-import { defineComponent  } from 'vue'
-import useDOMCreate from '@/hooks/useDOMCreate'
+import { defineComponent, onUnmounted } from 'vue'
+
 export default defineComponent({
-  props:{
-    text: String,
+  props: {
+    text: {
+      type: String
+    },
     background: {
       type: String
     }
   },
-  setup(){
-    useDOMCreate('back')
+  setup() {
+    const node = document.createElement('div')
+    node.id = 'back'
+    document.body.appendChild(node)
+    onUnmounted(() => {
+      document.body.removeChild(node)
+    })
   }
 })
 </script>
 
-<style scoped>
-   .loading-container{
-     background: rgba(255,255,255,.5);
-     z-index: 100;
-     position: fixed;
-     width: 100%;
-     height: 100%;
-     top:0;
-     left:0;
-   }
-   .loading-content{
-     text-align: center;
-   }
+<style>
+.loading-container {
+  background: rgba(255, 255, 255, .5);
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+.loading-container {
+  text-align: center;
+}
 </style>
