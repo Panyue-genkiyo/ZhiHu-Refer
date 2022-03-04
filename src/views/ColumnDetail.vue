434b2abf2451e1c8cae4e0855935261d3c6eadfb
panyue <90938238g@gmail.com>
2022-03-04 15:56:29 +0800

    专栏详情

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,44 +1,46 @@
 <template>
   <div class="column-detail-page w-75 mx-auto">
-    <div
-      class="column-info row mb-4 border-bottom pb-4 align-items-center"
-      v-if="column"
-    >
+    <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
       <div class="col-3 text-center">
-        <img
-          :src="column.avatar?.url"
-          :alt="column.title"
-          class="rounded-circle border w-100"
-        />
+        <img :src="column.avatar && column.avatar.fitUrl" :alt="column.title" class="rounded-circle border w-100">
       </div>
       <div class="col-9">
-        <h4>{{ column.title }}</h4>
-        <p class="text-muted">{{ column.description }}</p>
+        <h4>{{column.title}}</h4>
+        <p class="text-muted">{{column.description}}</p>
       </div>
     </div>
-    <post-list :list="list"/>
+    <post-list :list="list"></post-list>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted } from 'vue'
+import { defineComponent, computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
-
-import PostList from '@/components/PostList.vue'
+import { GlobalState, ColumnProps } from '@/store'
+import PostList from '../components/PostList.vue'
+import { addColumnAvatar } from '@/utils/helper'
 export default defineComponent({
-  name: 'ColumnDetail',
-  components: { PostList },
-  setup(){
-    const route = useRoute();
-    const store = useStore();
-    const currentId = route.params.id;
-    const column = computed(() => store.getters.getColumnById(currentId));
-    const list = computed(() => store.getters.getPostsByCid(currentId));
+  components: {
+    PostList
+  },
+  setup() {
+    const route = useRoute()
+    const store = useStore<GlobalState>()
+    const currentId = route.params.id
     onMounted(() => {
-      store.dispatch('fetchColumn', currentId);
-      store.dispatch('fetchPosts', currentId);
+      store.dispatch('fetchColumn', currentId)
+      store.dispatch('fetchPosts', currentId)
     })
+    const column = computed(() => {
+      const selectColumn = store.getters.getColumnById(currentId) as ColumnProps | undefined
+      if (selectColumn) {
+        addColumnAvatar(selectColumn, 100, 100)
+      }
+      return selectColumn
+    })
+    const list = computed(() => store.getters.getPostsByCid(currentId))
+
     return {
       column,
       list
