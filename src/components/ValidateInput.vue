c1ec3ba3039b17662d8c275f8536943ecb4f5351
panyue <90938238g@gmail.com>
2022-03-04 15:52:37 +0800

    自定义组件input/textarea

--- src/components/ValidateInput.vue
+++ src/components/ValidateInput.vue
@@ -1,101 +1,93 @@
 <template>
   <div class="validate-input-container pb-3">
-     <input
-            v-if="tag !== 'textarea'"
-            class="form-control"
-            :class="{'is-invalid': inputRef.error}"
-            :value="inputRef.val"
-            @input="updateValue"
-            @blur="validateInput"
-            v-bind="$attrs"
-     />
+    <input
+      v-if="tag !== 'textarea'"
+      class="form-control"
+      :class="{'is-invalid': inputRef.error}"
+      @blur="validateInput"
+      v-model="inputRef.val"
+      v-bind="$attrs"
+    >
     <textarea
       v-else
       class="form-control"
       :class="{'is-invalid': inputRef.error}"
-      :value="inputRef.val"
-      @input="updateValue"
       @blur="validateInput"
+      v-model="inputRef.val"
       v-bind="$attrs"
-    />
-     <span v-if="inputRef.error" class="invalid-feedback">{{ inputRef.message }}</span>
+    >
+    </textarea>
+    <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, reactive, onMounted } from 'vue'
+import { defineComponent, reactive, PropType, onMounted, computed } from 'vue'
 import { emitter } from './ValidateForm.vue'
-const emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-interface RuleProp{
-  type: 'required' | 'email' | 'minLength' | 'maxLength' | 'custom',
+
+// 邮箱校验格式
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+interface RuleProp {
+  // 校验格式类型
+  type: 'required' | 'email' | 'custom';
   message: string;
-  validator?: () => boolean
+  validator?: () => boolean;
 }
-export type RulesProp = RuleProp[];
-export type TagType = 'input' | 'textarea';
+export type RulesProp = RuleProp[]
+export type TagType = 'input' | 'textarea'
 export default defineComponent({
-  name: 'ValidateInput',
-  inheritAttrs: false,
-  props:{
-    rules: {
-      type: Array as PropType<RulesProp>,
-    },
+  props: {
+    rules: Array as PropType<RulesProp>,
     modelValue: String,
-    tag:{
+    tag: {
       type: String as PropType<TagType>,
       default: 'input'
     }
   },
+  inheritAttrs: false,
   setup(props, context) {
     const inputRef = reactive({
-      val: props.modelValue || '',
-      error:false,
+      val: computed({
+        get: () => props.modelValue || '',
+        set: val => {
+          context.emit('update:modelValue', val)
+        }
+      }),
+      error: false,
       message: ''
-    });
-    const updateValue = (e: KeyboardEvent) => {
-      const targetValue = (e.target as HTMLInputElement).value;
-      inputRef.val = targetValue;
-      //自定义组件支持v-model发送事件
-      context.emit('update:modelValue', targetValue);
-    };
+    })
     const validateInput = () => {
-      if(props.rules){
-        const allPass = props.rules.every(rule => {
-           let passed = true;
-           inputRef.message = rule.message;
-           switch(rule.type){
-             case 'required':
-               passed = inputRef.val.trim().length > 0;
-               break;
-             case 'email':
-               passed = emailReg.test(inputRef.val);
-               break;
-             case 'minLength':
-               passed = inputRef.val.length >= 6;
-               break;
-             case 'maxLength':
-               passed = inputRef.val.length <= 12;
-               break;
-             case 'custom':
-               passed = rule.validator ? rule.validator() : true;
-               break;
-             default:
-               break;
-           }
-           return passed;
-        });
-        inputRef.error = !allPass;
-        return allPass;
+      if (props.rules) {
+        const allPassed = props.rules.every(rule => {
+          let passed = true
+          inputRef.message = rule.message
+          switch (rule.type) {
+            case 'required':
+              passed = (inputRef.val.trim() !== '')
+              break
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break
+            case 'custom':
+              passed = rule.validator ? rule.validator() : true
+              break
+            default:
+              break
+          }
+          return passed
+        })
+        inputRef.error = !allPassed
+        return allPassed
       }
-      return true;
+      return true
     }
     onMounted(() => {
-      emitter.emit('form-item-created', validateInput);
-    });
+      emitter.emit('form-item-created', validateInput)
+    })
     return {
       inputRef,
-      validateInput,
-      updateValue
+      validateInput
     }
   }
 })
