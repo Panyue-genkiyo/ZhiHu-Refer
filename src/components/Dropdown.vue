d67bdc97bf0e0963969d918d37c8b3912631b555
panyue <90938238g@gmail.com>
2022-03-04 15:50:37 +0800

    下拉菜单组件

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,47 +1,43 @@
 <template>
-  <div class="dropdown" ref="dropDownRef">
+  <div class="dropdown" ref="dropdownRef">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
-      {{ title }}
+      {{title}}
     </a>
-    <ul class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
-       <slot></slot>
+    <ul class="dropdown-menu" @click="toggleOpen" :style="{display: 'block'}" v-if="isOpen">
+      <slot></slot>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, watch } from 'vue'
-import useClickOutside from '@/hooks/useClickOutside'
+import useClickOutside from '../hooks/useClickOutside'
 export default defineComponent({
   name: 'Dropdown',
-  props:{
+  props: {
     title: {
       type: String,
       required: true
     }
   },
-  setup(){
-    const isOpen = ref(false);
-    const dropDownRef = ref<null | HTMLElement>(null); //拿到dropdown的ref节点
-
+  setup() {
+    const isOpen = ref(false)
+    const dropdownRef = ref<null | HTMLElement>(null)
     const toggleOpen = () => {
-      isOpen.value = !isOpen.value;
+      isOpen.value = !isOpen.value
+      console.log(isOpen.value)
     }
-
-    let isClickOutside = useClickOutside(dropDownRef);
+    const isClickOutside = useClickOutside(dropdownRef)
 
     watch(isClickOutside, () => {
-       console.log('isClickOutside', isClickOutside.value);
-       // console.log(isOpen.value);
-       if(isClickOutside.value && isOpen.value){
-         isOpen.value = false;
-       }
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
+      }
     })
-
     return {
       isOpen,
       toggleOpen,
-      dropDownRef
+      dropdownRef
     }
   }
 })
